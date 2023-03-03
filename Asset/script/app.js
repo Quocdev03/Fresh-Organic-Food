3aafe5675a04ebfead0020d528c613c5cc216531
Cao Chi Quoc <ccquoc2100346@student.ctuet.edu.vn>
2023-03-03 20:53:45 +0700

    responsive heade. add menu toggle

---
+++ Asset/script/app.js
@@ -0,0 +1,15 @@
+// Menu Toggle
+// Lấy giá trị bên html
+const menuOpen = document.querySelector(".menu-toggle");
+const menuClose = document.querySelector(".menu-close");
+const mbNav = document.querySelector(".mb-nav");
+const activeClass = "is-show";
+
+// click vào menu toggle thì thêm class is-show
+menuOpen.addEventListener("click", function () {
+   mbNav.classList.add(activeClass);
+})
+// click vào menu close thì thêm class is-show
+menuClose.addEventListener("click", function () {
+   mbNav.classList.remove(activeClass);
+})
