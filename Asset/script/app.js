0c69010ae423d34422a6996e072f9f3ebcdaaa7c
Cao Chi Quoc <ccquoc2100346@student.ctuet.edu.vn>
2023-03-03 20:54:29 +0700

    add menu toggle

--- Asset/script/app.js
+++ Asset/script/app.js
@@ -4,12 +4,11 @@ const menuOpen = document.querySelector(".menu-toggle");
 const menuClose = document.querySelector(".menu-close");
 const mbNav = document.querySelector(".mb-nav");
 const activeClass = "is-show";
-
 // click vào menu toggle thì thêm class is-show
 menuOpen.addEventListener("click", function () {
    mbNav.classList.add(activeClass);
 })
-// click vào menu close thì thêm class is-show
+// click vào menu close thì xoá class is-show
 menuClose.addEventListener("click", function () {
    mbNav.classList.remove(activeClass);
 })
